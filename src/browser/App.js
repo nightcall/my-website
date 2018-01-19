51c563b30ea260d38247ea0282c12b4ffa4990be
saraband <sara@band.net>
2018-01-19 18:43:50 +0100

    commit

--- src/browser/App.js
+++ src/browser/App.js
@@ -13,29 +13,6 @@ export default class App extends React.Component {
 
 	constructor(props) {
 		super(props);
-
-		this.state = {
-			currentProject: -1,
-			showProject: true
-		};
-
-		this.projects = [
-			{
-				id: 0,
-				title: 'Project 0',
-				description: 'This is projct 1 blabla bla'
-			},
-			{
-				id: 1,
-				title: 'Project 1',
-				description: 'This is projct 2 blabla bla'
-			},
-			{
-				id: 2,
-				title: 'Project 2',
-				description: 'This is projct 3 blabla bla'
-			}
-		];
 	}
 
 	getChildContext() {
--- src/browser/Translations.js
+++ src/browser/Translations.js
@@ -6,10 +6,16 @@ const translations = {
 		'navbar-contact': 'Contact'
 	},
 	fr: {
-		'navbar-home': 'Accueil'
+		'navbar-home': 'Accueil',
+		'navbar-projects': 'Projets',
+		'navbar-about': 'A propos',
+		'navbar-contact': 'Contact'
 	},
 	es: {
-		'navbar-home': 'Inicio'
+		'navbar-home': 'Inicio',
+		'navbar-projects': 'Proyectos',
+		'navbar-about': 'Acerca de mi',
+		'navbar-contact': 'Contacto'
 	}
 };
 
