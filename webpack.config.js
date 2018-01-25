6968e0c3320fe2beb8ffcffe83d7683f776d9776
saraband <sara@band.com>
2018-01-25 12:51:51 +0100

    commit

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,8 @@ module.exports = {
         'process.env': {
           'NODE_ENV': JSON.stringify('production')
         }
-      })
+      }),
+      new webpack.optimize.UglifyJsPlugin()
     ],
     module: {
         rules: [
