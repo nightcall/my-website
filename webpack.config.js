4430045b71f66e886c927f81b05c0da3d34d3bc8
saraband <sara@band.com>
2018-01-25 12:48:02 +0100

    commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,18 @@
+const webpack = require('webpack');
+
 module.exports = {
     entry: './src/browser/index.js'  ,
     output: {
         path: __dirname + '/public',
         filename: 'bundle.js'
     },
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env': {
+          'NODE_ENV': JSON.stringify('production')
+        }
+      })
+    ],
     module: {
         rules: [
             {
